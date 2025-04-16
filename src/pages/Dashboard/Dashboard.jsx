e9df09414ac2694d056e13cc1cbcf3f91efdb906
moizghumann <moizghuman@gmail.com>
2025-04-16 21:30:14 +0500

    Refactor Dashboard component by removing unused imports and optimizing the code structure for better readability.

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,4 @@
-import React, { useState } from "react";
-import { MdOutlineShowChart, MdSpaceDashboard } from "react-icons/md";
-import { HiMiniShoppingCart, HiUsers } from "react-icons/hi2";
-import { AiFillProduct } from "react-icons/ai";
-import { IoBagCheck, IoStatsChartSharp } from "react-icons/io5";
-import { FaUser, FaWallet } from "react-icons/fa";
-import { IoMdSettings } from "react-icons/io";
-import ApexLineChart from "../../components/Charts/LineChart";
-import DashboadCard from "../../components/Cards/DashboadCard";
-import { RiComputerFill } from "react-icons/ri";
-import { FaSackDollar } from "react-icons/fa6";
-import { Popover, Typography } from "@mui/material";
-import { BiSolidChevronDown } from "react-icons/bi";
+import React from "react";
 import SideMenu from "../../components/SideMenu/SideMenu";
 import DashboadAllSalesChart from "../../components/Charts/DashboadAllSalesChart";
 import DashboardCards from "../../components/Cards/DashboardCards";
